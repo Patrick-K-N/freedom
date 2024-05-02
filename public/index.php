9981032aee2f5471d0ba0b7ee385391774162c55
Patrick-K-N <patricknjuguna621@gmail.com>
2024-05-02 08:55:28 +0300

    code modify

--- public/index.php
+++ public/index.php
@@ -1,17 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Blade;
 
-define('LARAVEL_START', microtime(true));
+$content = Blade::render('welcome');
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+echo $content;
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+?>
